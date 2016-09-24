b203dc4791f69ff5654152ff5a2e533bd5a4e4c1
dencesun <sunxiaowei199325@163.com>
2016-09-24 22:49:46 +0800

    165

--- 165CompareVersion.py
+++ 165CompareVersion.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 class Solution(object):
     def compareVersion(self, version1, version2):
         """
@@ -14,5 +16,23 @@ class Solution(object):
         return cmp(v1,v2)
 
 
-test = Solution()
-print test.compareVersion('0.01.0.1', '0.0.1')
+# test = Solution()
+# print test.compareVersion('0.01.0.1', '0.0.1')
+
+# map补充
+
+class mapLearn(object):
+	def add100(self, x):
+		return x+100
+	def abc(self, a, b, c):
+		return a*10000+b*100+c
+	def test(self):
+		list1 = [11,22,33]
+		list2 = [44,55,66]
+		list3 = [77,88,99]
+		print map(self.add100, list1)
+		print map(self.abc, list1, list2, list3)
+		print map(None, list1, list2, list3)
+test = mapLearn()
+test.test()
+
