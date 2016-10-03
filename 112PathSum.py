554cf2793abe072c64d9394fe4b72361af2ee312
dencesun <sunxiaowei199325@163.com>
2016-10-03 13:17:34 +0800

    new112

--- 112PathSum.py
+++ 112PathSum.py
@@ -7,8 +7,25 @@
 
 class Solution(object):
     def hasPathSum(self, root, sum):
-        """
-        :type root: TreeNode
-        :type sum: int
-        :rtype: bool
-        """
+# 方法1
+        # result = []
+        # if root is None:
+        # 	return False
+        # def dfs(root, ans):
+        # 	if root.left is None and root.right is None:
+        # 		result.append(ans)
+        # 	if root.left:
+        # 		dfs(root.left, ans+root.left.val)
+        # 	if root.right:
+        # 		dfs(root.right, ans+root.right.val)
+       	# dfs(root, root.val)
+        # if sum in result:
+        # 	return True
+        # return False
+# 方法2
+		if root is None: 
+			return False
+		if root.left is None and root.right is None:
+			return root.val == sum
+		return self.hasPathSum(root.left, sum - root.val) or self.hasPathSum(root.right, sum-root.val)
+
