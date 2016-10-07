3ab136c0bb36031505d81d281a47c03741c5825c
dencesun <sunxiaowei199325@163.com>
2016-10-07 11:16:24 +0800

    347

---
+++ 347TopKelements.py
@@ -0,0 +1,11 @@
+from collections import Counter
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        count = Counter(nums)
+        return [x[0] for x in count.most_common(k)]
+
