b0a2ef8ee5f757adaf709ddf40b24d433d8c7e27
dencesun <sunxiaowei199325@163.com>
2016-10-12 22:58:16 +0800

    20

--- 20ValidParentheses.py
+++ 20ValidParentheses.py
@@ -1,25 +1,43 @@
 class Solution(object):
+    # def isValid(self, s):
+    #     """
+    #     :type s: str
+    #     :rtype: bool
+    #     """
+    #   	l = []
+
+    #   	for i in s:
+    #   		if i is '(' or i is '[' or i is '{':
+    #   			l.append(i)
+    #   		if i is ')' and len(l)>0 and l[-1] is '(':
+    #   			l.pop()
+    #    		if i is ']' and len(l)>0 and l[-1] is '[':
+    #   			l.pop()
+    #   		if i is '}' and len(l)>0 and l[-1] is '{':
+    #   			l.pop()
+
+    #   	if len(l)>0:
+    #   		return False
+    #   	return True
+
     def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-      	l = []
-
-      	for i in s:
-      		if i is '(' or i is '[' or i is '{':
-      			l.append(i)
-
-      		if i is ')' and l and l.pop() !='(':
-      			return False
-       		if i is ']' and len(l)>0 and l[-1] is '[':
-      			l.pop()
-      		if i is '}' and len(l)>0 and l[-1] is '{':
-      			l.pop()
-
-      	if len(l)>0:
-      		return False
-      	return True
-
+        stack = []
+        for i in range(len(s)):
+            if s[i] == '(' or s[i] == '[' or s[i] == '{':
+                stack.append(s[i])
+            if s[i] == ')':
+                if stack == [] or stack.pop() != '(':
+                    return False
+            if s[i] == ']':
+                if stack == [] or stack.pop() != '[':
+                    return False
+            if s[i] == '}':
+                if stack == [] or stack.pop() != '{':
+                    return False
+        if stack:
+            return False
+        else:
+            return True
+            
 test = Solution()
-print test.isValid("[")
+print test.isValid("()")
