a357aa1ef16f7faca748890d40bc8091eebc06bd
dencesun <sunxiaowei199325@163.com>
2016-10-13 23:34:38 +0800

    290

--- 290WordPatter.py
+++ 290WordPatter.py
@@ -5,7 +5,24 @@ class Solution(object):
         :type str: str
         :rtype: bool
         """
-        l1 = []
-        l2 = []
+        # word = str.split()
+        # pdict = {}
+        # wdict = {}
+        # ans = {}
+        # if len(pattern) != len(word):
+        # 	return False
+        # for k, v in zip(pattern, word):
+        # 	if k not in pdict:
+        # 		pdict[k] = v
+        # 	if v not in wdict:
+        # 		wdict[v] = k
+        # 	if pdict[k] != v or wdict[v]!=k:
+        # 		return False
+        # return True
 
-        for i in pattern:
+        # 方法2
+        words = str.split(' ')
+        if len(words) != len(pattern):
+            return False
+        return map(pattern.find, pattern) == map(words.index, words)
+
